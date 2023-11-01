b4962931707ac6f743effe7439d27591e9a9bc00
mageoffiree <119866995+mageoffiree@users.noreply.github.com>
2023-11-01 20:40:54 +0300

    50% рабочий огрызок кода

--- Part1.sh
+++ Part1.sh
@@ -5,8 +5,10 @@ bMain=true
 while $bMain;
 do
 
+	echo
 	echo "Введите номер группы [A-XX-XX]:"
 	echo "0, чтобы вернуться к выбору части задания"
+	echo
 
 	read group
 
@@ -17,10 +19,12 @@ do
 		bTask10=true
 		while $bTask10;
 		do
+			echo
 			echo "Необходимо выбрать действие:"
 			echo "1 - Студенты, которые не сдали все с первой попытки"
 			echo "2 - Упорядоченный список группы"
 			echo "0 - Вернуться к введению номера группы"
+			echo
 
 			read Option
 
@@ -28,10 +32,21 @@ do
 			"0") bTask10=false;;
 
 			"1")
-			grep -r (;2$) ./labfiles/*/tests/TEST-*)
-
+			echo
+			echo "Криптозоология:"
+			egrep \(^\($group\).*2$\) ./labfiles/Криптозоология/tests/TEST-* |  sed -e 's/^.*\/T/T/g' -e 's/.A-..-.../ /g' -e 's/..20[1,2][1-9].*$//g' | sort -u
+			echo
+			echo "Пивоварение:"
+			egrep \(^\($group\).*2$\) ./labfiles/Пивоварение/tests/TEST-* |  sed -e 's/^.*\/T/T/g' -e 's/.A-..-.../ /g' -e 's/..20[1,2][1-9].*$//g' | sort -u
+			
+			esac
+			done
+			;;
 			"2")
 
+			esac
+			done
+			;;
 			*)
 			echo "Ошибка: Нет такой команды. Повторите ввод."
 			;;
@@ -43,9 +58,3 @@ do
 	esac
 
 done
-
-
-
-
-
-
