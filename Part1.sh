c4fe6ebfad02ece0663b7521c86afa4e5a6245a1
mageoffiree <119866995+mageoffiree@users.noreply.github.com>
2023-11-01 22:11:24 +0300

    60% работоспособного куска кода

--- Part1.sh
+++ Part1.sh
@@ -13,48 +13,62 @@ do
 	read group
 
 	case "$group" in
-		"0") bMain=false ;;
+		"0") 
+			bMain=false
+		;;
+
 		*)
+		buffer=$(find ./labfiles/Криптозоология/ -name "$group-attendance")
+		if [ -n "$buffer" ]
+		then
+			bTask10=true
+			while $bTask10;
+			do
+				echo
+				echo "Необходимо выбрать действие:"
+				echo "1 - Студенты, которые не сдали все с первой попытки"
+				echo "2 - Упорядоченный список группы"
+				echo "0 - Вернуться к введению номера группы"
+				echo
 
-		bTask10=true
-		while $bTask10;
-		do
-			echo
-			echo "Необходимо выбрать действие:"
-			echo "1 - Студенты, которые не сдали все с первой попытки"
-			echo "2 - Упорядоченный список группы"
-			echo "0 - Вернуться к введению номера группы"
-			echo
+				read Option
 
-			read Option
+				case "$Option" in
+					"0") 
+						bTask10=false
+					;;
 
-			case "$Option" in
-			"0") bTask10=false;;
+					"1")
+						echo
+						echo "Криптозоология:"
+						egrep \(^\($group\).*2$\) ./labfiles/Криптозоология/tests/TEST-* |  sed -e 's/^.*\/T/T/g' -e 's/.A-..-.../ /g' -e 's/..20[1,2][1-9].*$//g' | sort -u
+						echo
+						echo "Пивоварение:"
+						egrep \(^\($group\).*2$\) ./labfiles/Пивоварение/tests/TEST-* |  sed -e 's/^.*\/T/T/g' -e 's/.A-..-.../ /g' -e 's/..20[1,2][1-9].*$//g' | sort -u
+					;;
 
-			"1")
-			echo
-			echo "Криптозоология:"
-			egrep \(^\($group\).*2$\) ./labfiles/Криптозоология/tests/TEST-* |  sed -e 's/^.*\/T/T/g' -e 's/.A-..-.../ /g' -e 's/..20[1,2][1-9].*$//g' | sort -u
-			echo
-			echo "Пивоварение:"
-			egrep \(^\($group\).*2$\) ./labfiles/Пивоварение/tests/TEST-* |  sed -e 's/^.*\/T/T/g' -e 's/.A-..-.../ /g' -e 's/..20[1,2][1-9].*$//g' | sort -u
-			
-			esac
+					"2")
+						echo 
+						echo "Криптозоология:"
+
+						echo
+						echo "Пивоварение:"
+
+					;;
+
+					*)
+						echo "Ошибка: Нет такой команды. Повторите ввод."
+					;;
+
+				esac	
+
 			done
-			;;
-			"2")
-
-			esac
-			done
-			;;
-			*)
-			echo "Ошибка: Нет такой команды. Повторите ввод."
-			;;
 
 		else
 			echo "Ошибка: Группа не найдена."
 		fi
 		;;
+
 	esac
 
 done
